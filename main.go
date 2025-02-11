86f861a56f3204bf8a49875ef1101872c6788629
heinanX <larrii@live.se>
2025-02-11 23:12:35 +0100

    if statement

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	//possible way to declare a variable but not used
 	// var age = 34
 
 	// possible way to declare a variable and assign a value later
-	//var age int
+	//var age uint8 (uint8 for better performance as it takes less memory than default int)
 	//... code
 	//age = 34
 
 	age := 34 //declares and sets value at the same timeL
+	name := "Carly"
 
-	name := "Linda"
+	fmt.Printf("Hello %s. You seem to be %d years old\n", name, age)
 
-	fmt.Printf("Hello %s %d years old", name, age)
+	if strings.Contains(name, "Carly") {
+		fmt.Println("That's a good name")
+	} else {
+		fmt.Println("You should change")
+	}
 }
